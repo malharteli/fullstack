cb7077b55096650023a97897102ea34493613783
malharteli <malhar.teli@gmail.com>
2018-11-04 11:11:31 -0500

    Exercise Files from Javascript Essentials class
    Each class comes with exercise files structured by the class number. For this round, I'm only including the files that I coded with.

---
+++ Ex_Files_JavaScript_EssT/Exercise Files/03_02/script.js
@@ -0,0 +1,7 @@
+var negInteger = -3.14159265359;
+var escQuote = "Quotes can also be \"escaped\".";
+var theSunIsWarm = true;
+var emptyInside = null;
+var justAnotherVariable;
+
+console.log(typeof insertVariableName);
