a5e972c2dacbb9b0282df2e423ffdceeb000b1c3
malharteli <malhar.teli@gmail.com>
2019-01-20 19:57:51 -0500

    CSS update
    Implemented Loading Screen Functionality

---
+++ Website_HTML5CSS3/Smartmvp-Html-Template-1.2/New Main/assets/js/plugins/custom-malhar.js
@@ -0,0 +1,16 @@
+// Javascript Document
+
+/*=============================
+  LOADER
+===============================*/
+
+//makes sure the app is loaded
+$(window).load(function(){
+  "use strict";
+
+  //fade out loading animations
+  $(".signal").fadeOut();
+  //fade out preloader div
+  $(".preloader").fadeOut("slow")
+
+})
