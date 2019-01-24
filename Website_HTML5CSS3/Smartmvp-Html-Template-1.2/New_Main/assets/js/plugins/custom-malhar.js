6ba2251c30bd5a7cf9a82bc1ef8c56ec4bf6b163
malharteli <malhar.teli@gmail.com>
2019-01-23 20:59:06 -0500

    scrollTo Functionality implemented

--- Website_HTML5CSS3/Smartmvp-Html-Template-1.2/New_Main/assets/js/plugins/custom-malhar.js
+++ Website_HTML5CSS3/Smartmvp-Html-Template-1.2/New_Main/assets/js/plugins/custom-malhar.js
@@ -1,12 +1,13 @@
 // Javascript Document
 
+"use strict";
+
 /*=============================
   LOADER
 ===============================*/
 
 //makes sure the app is loaded
 $(window).load(function(){
-  "use strict";
 
   //fade out loading animations
   $(".signal").fadeOut();
@@ -19,7 +20,6 @@ $(window).load(function(){
   SCROLL NAVBAR
 ===============================*/
 $(window).scroll(function(){
-  "use strict";
 
   var top = $(window).scrollTop();
   if (top > 60){
@@ -33,7 +33,6 @@ $(window).scroll(function(){
   SPY For ACTIVE SECTION
 ===============================*/
 (function($){
-  "use strict";
   $('body').attr('data-spy', 'scroll').attr('data-target', '.navbar-fixed-top').attr('data-offset', '11');
 })(jQuery);
 
@@ -42,7 +41,6 @@ $(window).scroll(function(){
 ===============================*/
 
 (function($){
-  "use strict";
 
   $(".nav.navbar-nav li a").click(function(){
     var $togglebtn = $(".navbar-toggle");
@@ -52,3 +50,96 @@ $(window).scroll(function(){
   });
 
 })(jQuery);
+
+/*=============================*/
+/*=============================*/
+/*=============================
+  DOCUMENT READY
+  ===============================*/
+/*===============================*/
+/*===============================*/
+
+$(document).ready(function(){
+
+/*=============================
+  PARALLAX STELLAR WITH MOBILE FIXES
+===============================*/
+
+if(Modernizr.touch && ($(".header").attr("data-stellar-background-ratio")!== undefined)){
+  $('.header').css('background-attachment', 'scroll');
+  $('.header').removeAttr('data-stellar-background-ratio');
+} else {
+  $(window).stellar({
+    horizontalScrolling:false
+  });
+}
+
+/*=============================
+  WOW ANIMATIONS
+===============================*/
+new WOW().init();
+
+/*=============================
+  VIDEO BACKGROUND
+===============================*/
+$("#top").vide("assets/images/video/ocean",{
+  posterType: "jpg"
+});
+
+/*=============================
+  SCROLLTO
+===============================*/
+var onMobile;
+
+onMobile=false;
+if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)){onMobile=true;}
+if(onMobile  == true){
+  jQuery("a.scrollto").click(function(event){
+    jQuery("html,body").scrollTo(this.hash, this.hash, {gap:{y:-10}, animation: {easing:'easeInOutCubic', duration:0}});
+    event.preventDefault();
+  });
+}else {
+  jQuery("a.scrollto").click(function(event){
+    jQuery("html, body").scrollTo(this.hash, this.hash, {gap:{y:-10}, animation: {easing:'easeInOutCubic', duration: 1500}});
+    event.preventDefault();
+  })
+}
+
+/*=============================
+  SIGNUP-DIVIDER ANIMATED POLYGON BACKGROUND
+===============================*/
+
+  var container = document.getElementById('canvas-bg');
+  var renderer = new FSS.CanvasRenderer();
+  var scene = new FSS.Scene();
+  var light = new FSS.Light('323A45', '323A45');
+  var geometry = new FSS.Plane(2000, 1000, 15, 8);
+  var material = new FSS.Material('FFFFFF','FFFFFF');
+  var mesh = new FSS.Mesh(geometry, material);
+  var now, start = Date.now();
+
+  function initialise(){
+    scene.add(mesh);
+    scene.add(light);
+    container.appendChild(renderer.element);
+    window.addEventListener('resize', resize);
+  }
+
+  function resize(){
+    renderer.setSize(container.offsetWidth, container.offsetHeight);
+  }
+
+  function animate(){
+    now = Date.now() - start;
+    light.setPosition(300*Math.sin(now*0.001), 200*Math.cos(now*0.0005), 60);
+    renderer.render(scene);
+    requestAnimationFrame(animate);
+  }
+
+  initialise();
+  resize();
+  animate();
+
+
+
+})
